8aea17b21dc4a8c04520757e5303fbdbb7b0c73c
HARISREE SURESH <getharisree@outlook.com>
2023-05-08 17:04:24 +0530

    Update age-eligibility.js

--- age-eligibility.js
+++ age-eligibility.js
@@ -1,1 +1,10 @@
+const age = 
+const limit = 18
 
+if (age >= limit) {
+  console.log(`you are eligible for the licence`)
+}
+else {diff =  limit - age
+      console.log(`you have to wait ${diff} to get a licence`)
+     
+     }
