4e789a9b938011fee6c0d2856997e4bbbed038a0
HARISREE SURESH <getharisree@outlook.com>
2023-05-09 19:10:16 +0530

    Update operators_and_logic.js
    Using comparison operators and logic, find the winning team with conditions provided

--- operators_and_logic.js
+++ operators_and_logic.js
@@ -1,1 +1,10 @@
+let avg_dol = (97 + 112 + 89)/3
+let avg_kol = (109 + 95 + 106)/3
 
+console.log(avg_dol)
+console.log(avg_kol)
+
+if (avg_dol>100 && avg_dol > avg_kol){console.log(`dol had won the match`)}
+else if(avg_kol > 100 && avg_kol > avg_dol){console.log(`kol had won the match`)}
+else if(avg_dol && avg_kol > 100 && avg_dol === avg_kol){console.log(`game is in draw`)}
+else {console.log(`nobody won`)}
