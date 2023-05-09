22d3ae543afc575c0b67fa14d49691e3fc0f3842
HARISREE SURESH <getharisree@outlook.com>
2023-05-09 21:39:42 +0530

    Update conditional_operator(ternary).js

--- conditional_operator(ternary).js
+++ conditional_operator(ternary).js
@@ -1,1 +1,8 @@
+const bill = 275
+let tip = 0
 
+
+
+tip = bill >= 50 && bill <= 300 ? bill * (15/100) :  bill * (20/100)
+
+console.log(`the bill was ${bill} and your tip is ${tip} , in total you have to pay ${bill +tip}`)
