2b3dee82d1bc0cb88056b29463fc3ece0019f3c8
HARISREE SURESH <getharisree@outlook.com>
2023-05-09 20:27:06 +0530

    Update using_switch_statement.js

--- using_switch_statement.js
+++ using_switch_statement.js
@@ -1,1 +1,27 @@
+let day = "thurs"
+
+switch(day){
+    case "monday" :
+        console.log(`monday is selected`)
+        break
+    case "tuesday":
+        console.log(`tuesday is selected`)
+        break
+    case "wednesday":
+        console.log(`wednesday is selected`)
+        break
+    case "thursday":
+    case "friday":
+        console.log(`friday is selected`)
+        break
+    case "saturday":
+        console.log(`saturday is selected`)
+        break
+    case "sunday":
+        console.log(`should take a break`)
+        break
+    default:
+        console.log("not a valid day")
+
+}
 
