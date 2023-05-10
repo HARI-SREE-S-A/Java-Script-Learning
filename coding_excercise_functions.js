52f6a0bae56b8a2c07442f0f4e126c992b817c00
HARISREE SURESH <getharisree@outlook.com>
2023-05-10 19:32:48 +0530

    Update coding_excercise_functions.js
    Find the winner by finding the average of the score , but the minimum condition is that the  winner must have double the score of the opposite teams average score

--- coding_excercise_functions.js
+++ coding_excercise_functions.js
@@ -1,1 +1,18 @@
+const averag = (a,b,c) => (a+b+c)/3
 
+function winner (a,b,c,d,e,f){
+
+    dol_scr = averag(a,b,c)
+    kol_scr = averag(d,e,f)
+    console.log(dol_scr,kol_scr)
+
+    if (dol_scr >= 2*kol_scr){
+        console.log(`team dol won with ${dol_scr,kol_scr}`)}
+        else if (kol_scr >= 2*dol_scr){
+            console.log(`kol won the match`)
+        } else{console.log(`no body won the match${kol_scr,dol_scr}`)}
+    
+}
+
+winner(44,23,71,65,54,49)
+winner(85,54,41,23,34,27)
