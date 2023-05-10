71e00d87d1c42d8219cd38521353691e50ac64e4
HARISREE SURESH <getharisree@outlook.com>
2023-05-10 18:35:19 +0530

    Update Nesting_Functions.js

--- Nesting_Functions.js
+++ Nesting_Functions.js
@@ -1,1 +1,15 @@
+//nesting the functions
 
+const cutting_machine = fruit => {
+    return (fruit * 4)
+} 
+
+function juicer(apple,orange){
+    apple_cut = cutting_machine(apple)
+    orange_cut = cutting_machine(orange)
+
+    console.log(`juice is made from ${apple_cut} pieces of apple and ${orange_cut} pieces of orange`)
+}
+
+
+juicer(2,3)
