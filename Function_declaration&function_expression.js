63ea06c1db8b9eda44ea6f4241d7339c1e5067e5
HARISREE SURESH <getharisree@outlook.com>
2023-05-10 18:03:15 +0530

    Update Function_declaration&function_expression.js

--- Function_declaration&function_expression.js
+++ Function_declaration&function_expression.js
@@ -1,1 +1,22 @@
+// function declaration
 
+function agecalc1(year){
+    age = 2023 - year
+    return age
+
+}
+
+//function expression
+
+
+const age2 = function(year) {
+    age = 2023 - year
+    return age
+}
+
+// xollecting the results
+
+const v = agecalc1(1991)
+console.log(v)
+
+console.log(age2(1998))
