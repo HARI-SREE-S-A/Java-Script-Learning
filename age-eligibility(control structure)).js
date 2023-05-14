567480d168ed6996f44af3a140ed5f03609403e1
HARISREE SURESH <getharisree@outlook.com>
2023-05-14 09:57:18 +0530

    Update age-eligibility(control structure)).js

--- age-eligibility(control structure)).js
+++ age-eligibility(control structure)).js
@@ -3,6 +3,7 @@ const limit = 18
 
 if (age >= limit) {
   console.log(`you are eligible for the licence`)
+  
 }
 else {diff =  limit - age
       console.log(`you have to wait ${diff} to get a licence`)
