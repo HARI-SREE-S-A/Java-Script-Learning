b2ac253b6cce57365fa00ac62fa256d02c77fb2e
HARISREE SURESH <getharisree@outlook.com>
2023-05-15 20:51:17 +0530

    Update bmi_calc_challange3.js

--- bmi_calc_challange3.js
+++ bmi_calc_challange3.js
@@ -1,1 +1,33 @@
+const mark = {
+    name:"mark",
+    mass: 78,
+    height: 1.69,
+    cal_index:function(mass,height){
+        this.index = ((mass)/(height**2))
+        return this.index
+    }
+
+
+
+
+}
+
+mark.cal_index(mark.mass,mark.height)
+
+const john = {
+    name:"john",
+    mass: 92,
+    height: 1.95,
+    cal_index:function(mass,height){
+        this.index = ((mass)/(height**2))
+        return this.index
+    }
+
+
+}
+
+john.cal_index(john.mass,john.height)
+
+
+console.log(`the winner is ${mark.index > john.index ? `mark ` : 'john' } and he differ by ${mark.index} and ${john.index}`)
 
