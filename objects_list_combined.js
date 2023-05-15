82dda80f3d9ef000925f83683983e6ec031277a9
HARISREE SURESH <getharisree@outlook.com>
2023-05-15 19:58:46 +0530

    Update objects_list_combined.js

--- objects_list_combined.js
+++ objects_list_combined.js
@@ -1,1 +1,25 @@
+const avai_juice = {
+    apple:100,
+    orange:300,
+    lemon:15
+};
 
+const water = ["warm","cold","normie"]
+
+console.log(water.length)
+
+const name = prompt(`enter apple or orange or lemon`)
+
+const temp = prompt(`enter warm cold normie`)
+
+
+
+const wat_t = (temp) => {if (temp === "warm"){return water[0]}
+else if (temp === "cold"){return water[1]}
+else if(temp === "normie"){return water[2]}}
+
+
+
+if (avai_juice[name]){
+console.log(`you have selected ${name} juice in ${wat_t(temp)} and it cost ${avai_juice[name]} out of ${water.length}` )}
+else{console.log(`sorry that item is not available yet!!`)}
