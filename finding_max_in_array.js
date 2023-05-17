1c29458f2c1ac73fcd020f68cd72c99f741cfe8c
HARISREE SURESH <getharisree@outlook.com>
2023-05-17 18:16:42 +0530

    Update finding_max_in_array.js

--- finding_max_in_array.js
+++ finding_max_in_array.js
@@ -1,1 +1,15 @@
+"use strict";
+
+const a = [1, 2, 3, 4, 5, 6, 7];
+
+let max = a[0];
+for (let i = 0; i <= a.length - 1; i += 1) {
+  if (a[i] > max) {
+    max = a[i];
+  } else {
+    continue;
+  }
+}
+
+console.log(max);
 
