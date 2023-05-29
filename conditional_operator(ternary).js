7e6a83d93d77c64440abfc6c4304cde8b145f118
HARISREE SURESH <getharisree@outlook.com>
2023-05-29 21:00:54 +0530

    Update conditional_operator(ternary).js

--- conditional_operator(ternary).js
+++ conditional_operator(ternary).js
@@ -3,7 +3,6 @@ let tip = 0
 
 
 
-
 tip = bill >= 50 && bill <= 300 ? bill * (15/100) :  bill * (20/100)
 
 console.log(`the bill was ${bill} and your tip is ${tip} , in total you have to pay ${bill +tip}`)
