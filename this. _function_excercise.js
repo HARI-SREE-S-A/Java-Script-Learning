f609c2e797303b589fe24bae6b129649454dbca4
HARISREE SURESH <getharisree@outlook.com>
2023-05-29 20:56:25 +0530

    Update this. _function_excercise.js

--- this. _function_excercise.js
+++ this. _function_excercise.js
@@ -5,6 +5,7 @@ const hari = {
     friend : ["abe","joe","ram"],
     cal_age : function(year){
        
+        
         this.age =  2023 - year
         return this.age},
     summary: function(year){console.log(`name is ${this.fname + this.sname} age is ${this.cal_age(year)}
