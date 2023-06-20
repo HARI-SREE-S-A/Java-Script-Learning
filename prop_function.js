96680ec08f612436f81e64952cd88bef1b3a6139
HARISREE SURESH <getharisree@outlook.com>
2023-06-20 21:15:57 +0530

    Update prop_function.js

--- prop_function.js
+++ prop_function.js
@@ -1,1 +1,6 @@
-
+<div className="projectcard">
+      <img src={props.imgsrc} alt="image" />
+      <h2 className="Projecttitle">{props.title}</h2>
+      <div className="projdetails">
+        <p>{props.text}</p>
+        <div className="pro-btns">
